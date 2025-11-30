361ba8921de6a3646981d6a681fcd7005d8c5f29
ydunwye <85780277+ydunwye@users.noreply.github.com>
2025-11-30 18:02:59 +0900

    Add header file P5_update.h with function declarations

---
+++ problemSolving_group_H/P5_update.h
@@ -0,0 +1,14 @@
+// P5_update.h
+#ifndef P5_UPDATE_H
+#define P5_UPDATE_H
+
+#include "P1_sort.h"
+#include "P2_list.h"
+
+// P5-1
+void removeChoiCanceled_Array(Registration arr[], int n);
+
+// P5-2
+void removeChoiCanceled_List(Node** head);
+
+#endif
